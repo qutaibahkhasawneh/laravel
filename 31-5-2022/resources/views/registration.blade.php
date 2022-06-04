7dcbf2715e51b7eaaf5217d4769fc76f823cb5d3
qutaibahkhasawneh <100359595+qutaibahkhasawneh@users.noreply.github.com>
2022-06-04 19:50:36 +0300

    Add files via upload

---
+++ 31-5-2022/resources/views/registration.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/registratio.css" rel="stylesheet">
+    <title>Register Form</title>
+
+
+</head>
+<body >
+    <form action="" id="form">
+        <h2>Register</h2>
+        <div>
+            <input type="text"  id="" class="input" placeholder="Username">
+        </div> 
+        
+        <div>
+            <input type="email" id="" class="input" placeholder="Email Address">
+        </div>
+        <div>
+             <input type="password" id="" class="input" placeholder="Password">
+        </div>
+        <div>
+            <input type="password"  id="" class="input" placeholder="Confirm Password">
+        </div>
+        
+        <div>
+            <button id="register">REGISTER</button>
+        </div>
+
+         <div>
+            <button id="sing">Sign in</button>
+        </div>
+
+        <div>
+            <button id="google">SIGN UP WITH GOOGLE</button>
+        </div>
+
+        <div>
+             <button id="facebook">SIGN UP WITH FACEBOOK</button>
+        </div>
+
+    </form>
+</body>
+</html>
